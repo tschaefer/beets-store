05bf1c6e03de26024582cac594ae091d51156664
tschaefer <github@blackox.org>
2015-01-26 10:38:56 +0100

    beets-store: created setuptools stuff

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from setuptools import setup
+
+setup (
+        name = 'beets-store',
+        version = '0.0.1',
+        packages = ['beetsplug', 'beetsplug.store'],
+        author = 'Tobias Schäfer',
+        author_email = 'beets-store@blackoxorg',
+        url = 'https://github.com/tschaefer/beets-store',
+        description = "A plugin for the music geek's media organizer Beets",
+        license = 'BSD',
+        include_package_data=True
+)
