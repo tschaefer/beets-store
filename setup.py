481f5a97c43a3cd0217f260171aeefc146d279b7
tschaefer <github@blackox.org>
2015-03-20 11:42:36 +0100

    setup: added 'install_requires'

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ import sys
 from setuptools import setup
 
 setup (
-        name = 'beets-store',
-        version = '0.0.1',
-        packages = ['beetsplug', 'beetsplug.store'],
-        author = 'Tobias Schäfer',
-        author_email = 'beets-store@blackoxorg',
-        url = 'https://github.com/tschaefer/beets-store',
-        description = "A plugin for the music geek's media organizer Beets",
-        license = 'BSD',
+        name='beets-store',
+        version='0.0.1',
+        packages=['beetsplug', 'beetsplug.store'],
+        install_requires=['beets >= 1.3.10', 'Flask >= 0.10.1'],
+        author='Tobias Schäfer',
+        author_email='beets-store@blackoxorg',
+        url='https://github.com/tschaefer/beets-store',
+        description="A plugin for the music geek's media organizer Beets",
+        license='BSD',
         include_package_data=True
 )
